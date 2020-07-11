3a2363045c9e858ea4da6177a4ea27f92385c287
Changyun Lee <brownbearpower@gmail.com>
2020-07-11 22:22:08 +0900

    Updates

---
+++ precache-manifest.91e8b53259b348449ee7e2b1530a61f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff81333dc4067ce43ea4b425a76dc83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15c327a6d947b144f1ec",
+    "url": "/static/css/main.5492f6f6.chunk.css"
+  },
+  {
+    "revision": "f515f2040ee4cceb5565",
+    "url": "/static/js/2.5bff75b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5bff75b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c327a6d947b144f1ec",
+    "url": "/static/js/main.c0951593.chunk.js"
+  },
+  {
+    "revision": "8c50d92fcbdead60bf85",
+    "url": "/static/js/runtime-main.c29961dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lolchess/precache-manifest.45efb167ea9bb5fba8ecf7e0d1cf65fc.js"
+  "/precache-manifest.91e8b53259b348449ee7e2b1530a61f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lolchess/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
