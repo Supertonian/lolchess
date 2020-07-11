2edfc1916990bc6ef4e6888aa9f29ca6db28f779
Changyun Lee <brownbearpower@gmail.com>
2020-07-11 20:18:13 +0900

    Updates

---
+++ precache-manifest.2e7e599af5312d7ac71c8a0fb0e37d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a843ad617a3c1f74d40ce581bc5d638e",
+    "url": "/lolchess/index.html"
+  },
+  {
+    "revision": "560cb3a152da52b9df96",
+    "url": "/lolchess/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9c1f9aae485f951029fb",
+    "url": "/lolchess/static/js/2.93a35e57.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lolchess/static/js/2.93a35e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560cb3a152da52b9df96",
+    "url": "/lolchess/static/js/main.d4a3d9f7.chunk.js"
+  },
+  {
+    "revision": "f93699b6616e79556754",
+    "url": "/lolchess/static/js/runtime-main.b627c19a.js"
+  }
+]);
